522ee519b44f2955f64bc982792ec275f9963fcc
rajeshkumaryadav891341 <158378367+rajeshkumaryadav891341@users.noreply.github.com>
2024-04-09 23:46:08 -0400

    Update main.js
    story has been assigned to several variables.
    replacement of several items has been done.

--- Assignment 4/Part1/main.js
+++ Assignment 4/Part1/main.js
@@ -2,17 +2,18 @@ const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
+let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+let insertX = ['Willy the GoblinBig','DaddyFather','Christmas'];
+let insertY = ['the White House','Disneyland','the soup kitchen'];
+let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
+
+
 function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
   return array[random];
 }
 
-const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
-const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
-const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
-randomize.addEventListener('click', result);
 
 function result() {
   let newStory = storyText;
@@ -20,10 +21,8 @@ function result() {
   const xItem = randomValueFromArray(insertX);
   const yItem = randomValueFromArray(insertY);
   const zItem = randomValueFromArray(insertZ);
+  newStory = newStory.replace(':insertx:',xItem).replace(':insertx:',xItem).replace(':inserty:',yItem).replace(':insertz:',zItem);
 
-  newStory = newStory.replaceAll(':insertx:',xItem);
-  newStory = newStory.replaceAll(':inserty:',yItem);
-  newStory = newStory.replaceAll(':insertz:',zItem);
 
   if (customName.value !== '') {
     const name = customName.value;

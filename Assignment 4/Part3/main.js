bc4165961664f8eefa69f277e4e05609b29859b9
rajeshkumaryadav891341 <158378367+rajeshkumaryadav891341@users.noreply.github.com>
2024-04-10 00:48:59 -0400

    Update main.js
    gives idea of bounce heights

--- Assignment 4/Part3/main.js
+++ Assignment 4/Part3/main.js
@@ -43,3 +43,14 @@ class Ball {
     if (this.x - this.size <= 0) {
       this.velX = -this.velX;
     }
+    if (this.y + this.size >= height) {
+      this.velY = -this.velY;
+    }
+
+    if (this.y - this.size <= 0) {
+      this.velY = -this.velY;
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+  }

ae6edb83797883d053f38a340fda98a1b766c691
rajeshkumaryadav891341 <158378367+rajeshkumaryadav891341@users.noreply.github.com>
2024-04-10 00:57:12 -0400

    Update main.js
    this part of code removes ball drawing errors

--- Assignment 4/Part3/main.js
+++ Assignment 4/Part3/main.js
@@ -68,3 +68,20 @@ class Ball {
     }
   }
 }
+const balls = [];
+
+while (balls.length < 10) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size
+  );
+
+  balls.push(ball);
+}
